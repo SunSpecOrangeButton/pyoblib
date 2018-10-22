e9cb4496ed7f9c1319e04aa98d878e7a1bd81d0f
Joel <joel.e.boeder@wellsfargo.com>
2018-10-22 15:30:55 -0500

    Added echo statements to tests-cli.sh

--- scripts/tests-cli.sh
+++ scripts/tests-cli.sh
@@ -17,29 +17,37 @@ EXITVAL=0
 
 # Basic tests
 python src-cli/cli.py || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py -h || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py --help || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py info || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py version || {
+    echo Failed
     EXITVAL=1
 }
 
 # Identifier tests
 python src-cli/cli.py generate-identifier || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py validate-identifier 55db4ff3-5136-4be5-846b-4a93eb4c576d || {
+    echo Failed
     EXITVAL=1
 }
 python src-cli/cli.py validate-identifier 55db4ff3-5136-4be5-846b-4a93eb4c576 || {
+    echo Failed
     EXITVAL=1
 }
 
