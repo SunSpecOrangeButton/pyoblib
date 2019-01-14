9e114e9ade7b5ffcf1ce7c6547d0b144a06dea3a
Cliff Hansen <cwhanse@sandia.gov>
2019-01-14 13:39:14 -0700

    add test for each type validation function

--- oblib/tests/test_validator.py
+++ oblib/tests/test_validator.py
@@ -55,9 +55,46 @@ class TestValidator(unittest.TestCase):
         self.assertEqual(1, len(validator.validate_concept_value(concept, "Arf")))
         self.assertEqual(0, len(validator.validate_concept_value(concept, identifier.identifier())))
 
-
     def test_get_validator_method_name(self):
         type_name = "xbrli:booleanItemType"
         method_name_expected = "xbrli_boolean_item_type_validator"
         method_name_result = validator.get_validator_method_name(type_name)
         self.assertEqual(method_name_expected, method_name_result)
+
+    def test_xbrli_boolean_item_type_validator(self):
+        values = {True: (True, []), 'True': (True, []), 'true': (True, []),
+                  '1': (True, []), False: (False, []), 'False': (False, []),
+                  '0': (False, []),
+                  6: (6, ["'{}' is not a valid boolean value.".format(6)]),
+                  5.1: (5.1, ["'{}' is not a valid boolean value.".format(5.1)]),
+                  'foo': ('foo', ["'{}' is not a valid boolean value.".format('foo')])}
+        for v in values.keys():
+            result = validator.xbrli_boolean_item_type_validator(v)
+            self.assertEqual(result[0], values[v][0])
+            if values[v][1]: # error messages are present
+                self.assertTrue(result[1])
+            else:
+                self.assertFalse(result[1])
+
+    def test_xbrli_string_item_type_validator(self):
+        values = {'frog': ('frog', []), 'Frog': ('Frog', []),
+                  True: ('True', []), 0.3: ('0.3', [])}
+        for v in values.keys():
+            result = validator.xbrli_string_item_type_validator(v)
+            self.assertEqual(result[0], values[v][0])
+            if values[v][1]: # error messages are present
+                self.assertTrue(result[1])
+            else:
+                self.assertFalse(result[1])
+
+    def test_xbrli_integer_item_type_validator(self):
+        values = {6: (6, []), '5': (5, []), True: (1, []),
+                  'a': ('a', ["'{}' is not a valid integer value.".format('a')]),
+                  5.1: (5.1, ["'{}' is not a valid integer value.".format(5.1)])}
+        for v in values.keys():
+            result = validator.xbrli_integer_item_type_validator(v)
+            self.assertEqual(result[0], values[v][0])
+            if values[v][1]: # error messages are present
+                self.assertTrue(result[1])
+            else:
+                self.assertFalse(result[1])
