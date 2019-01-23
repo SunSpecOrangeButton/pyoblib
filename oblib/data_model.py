cfa060b71728732323311f030f288432b409e7c7
Cliff Hansen <cwhanse@sandia.gov>
2019-01-22 19:37:35 -0700

    another fix to OBInstance.set

--- oblib/data_model.py
+++ oblib/data_model.py
@@ -991,7 +991,7 @@ class OBInstance(object):
 
         if "context" in kwargs:
             context = kwargs.pop("context")
-        elif set(["duration", "instant", "entity"]) in kwargs.keys():
+        elif all([k in kwargs for k in {"duration", "instant", "entity"}]):
             # turn the remaining keyword args into a Context object -- this
             # is just syntactic sugar to make this method easier to call.
             # TODO this block will not work
