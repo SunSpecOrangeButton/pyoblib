4c1b706f55ccda12bd6a3213e5588d526e8c68a9
Joelebwf <joel.e.boeder@wellsfargo.com>
2019-02-26 09:20:35 -0600

    Fixed build (issue #149)

--- scripts/setup-dev.sh
+++ scripts/setup-dev.sh
@@ -19,6 +19,7 @@ mkdir oblib/data
 cd oblib/data
 git clone https://github.com/SunSpecOrangeButton/solar-taxonomy.git
 cd solar-taxonomy
+git checkout 568a75a7f11763b10a89a46f701a754df7c5c99c
 mkdir external
 curl http://xbrl.fasb.org/us-gaap/2017/elts/us-gaap-2017-01-31.xsd > external/us-gaap-2017-01-31.xsd
 curl https://xbrl.sec.gov/dei/2018/dei-2018-01-31.xsd > external/dei-2018-01-31.xsd
